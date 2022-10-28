4caa890894ab82d7231a407042b50231090a6d6e
KesterDaniel <kesterdan17@gmail.com>
2022-10-28 19:48:03 +0100

    fetched books data and passed them as props to home component

--- src/App.js
+++ src/App.js
@@ -9,13 +9,31 @@ class App extends Component{
     state = {
         currentlyReading: [],
         wantToRead: [],
-        Read: []
+        Read: [],
+        allbooks: []
+    }
+    async componentDidMount(){
+        const books = await BookApi.getAll()
+        const currentlyReading = books.filter((book)=>book.shelf ==="currentlyReading")
+        const wantToRead = books.filter((book)=>book.shelf ==="wantToRead")
+        const Read = books.filter((book)=>book.shelf ==="read")
+        this.setState({currentlyReading})
+        this.setState({wantToRead})
+        this.setState({Read})
+        console.log(this.state)
+        
     }
     render() {
         return (
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
+                    <Route path="/" element={
+                        <Home 
+                            currentlyReading={this.state.currentlyReading}
+                            wantToRead={this.state.wantToRead}
+                            Read={this.state.Read}
+                        />
+                    }/>
                     <Route path="/search" element={<Searchpage/>}/>
                 </Routes>
             </div>
